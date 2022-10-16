1d3d6e4afce5831ad4acd347e5f6f377a3736dc4
K D Hedger <kdhedger68713@gmail.com>
2022-10-16 14:12:14 +0100

    1st commit mountshare

---
+++ MountShare/resources/exec.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+
+#
+#©K. D. Hedger. Sun 16 Oct 14:01:47 BST 2022 keithdhedger@gmail.com
+#
+#This file (exec.sh) is part of MountShare.
+#
+#MountShare is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#MountShare is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with MountShare.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+$fm_import
+CONFIG="$fm_cmd_data"
+
+PASSFILE="${CONFIG}/pass"
+USERFILE="${CONFIG}/user"
+POPULATE="${fm_cmd_dir}/populatedrop"
+
+SERVERS="${CONFIG}/servers"
+IPADDRESS="${CONFIG}/ip"
+DROPDISK="${CONFIG}/disks"
+SELECTEDDISK="${CONFIG}/selecteddisk"
+CURRENTSERVER="${CONFIG}/currentserver"
+PORTNUM="${CONFIG}/portnum"
+PORTNUMSSH="${CONFIG}/portnumssh"
+PORTNUMFTP="${CONFIG}/portnumftp"
+PORTNUMSMB="${CONFIG}/portnumsmb"
+PORTNUMDAV="${CONFIG}/portnumdav"
+
+mkdir -p "$CONFIG" &>/dev/null
+
+export IPADDRESS
+export SERVERS
+export DROPDISK
+export SELECTEDDISK
+export CURRENTSERVER
+export PORTNUM
+export PORTNUMSSH
+export PORTNUMFTP
+export PORTNUMSMB
+export PORTNUMDAV
+
+if [ ! -e $IPADDRESS ];then
+	echo "127.0.0.0" > $IPADDRESS
+fi
+
+if [ ! -e $PASSFILE ];then
+	: > $PASSFILE
+fi
+
+if [ ! -e $USERFILE ];then
+	whoami>"$USERFILE"
+fi
+
+if [ ! -e $SERVERS ];then
+	echo " " > $SERVERS
+fi
+
+if [ ! -e $DROPDISK ];then
+	echo " " > $DROPDISK
+fi
+
+if [ ! -e $SELECTEDDISK ];then
+	echo " " > $SELECTEDDISK
+fi
+
+if [ ! -e $CURRENTSERVER ];then
+	echo " " > $CURRENTSERVER
+fi
+
+if [ ! -e $PORTNUMSSH ];then
+	echo "22" > $PORTNUMSSH
+fi
+
+if [ ! -e $PORTNUMFTP ];then
+	echo "21" > $PORTNUMFTP
+fi
+
+if [ ! -e $PORTNUMSMB ];then
+	echo "445" > $PORTNUMSMB
+fi
+
+if [ ! -e $PORTNUMDAV ];then
+	echo "8888" > $PORTNUMDAV
+fi
+
+cp "$PORTNUMSMB" "$PORTNUM"
+
+eval "$(spacefm -g --hbox --radio "FTP" "0" disable drop1 %v disable drop2 %v -- cp "$PORTNUMFTP" "$PORTNUM" -- disable freebutton1 %v -- --radio "SSH" "0" disable drop1 %v -- disable drop2 %v -- disable freebutton1 %v -- cp "$PORTNUMSSH" "$PORTNUM" -- --radio "DAV" "0" disable drop1 %v -- disable drop2 %v -- disable freebutton1 %v -- $POPULATE 'portnumdav' --radio "SMB" "1" enable drop1 %v -- enable drop2 %v -- enable freebutton1 %v -- cp "$PORTNUMSMB" "$PORTNUM" -- --close-box -- enable freebutton1 -- --label "IP Address" --hbox --free-button "Scan" -- $POPULATE 'server' --drop "@$SERVERS" "@$CURRENTSERVER" -- $POPULATE 'lookup' %v  --input  @$IPADDRESS -- $POPULATE 'disks' %v --close-box --label "Mount Point" --drop @$DROPDISK "@$SELECTEDDISK" --label "User Name" --input "@$USERFILE" --label "Password" --password @$PASSFILE --label "Port[/SHARE/] " --input "@$PORTNUM" --button Mount:gtk-apply --button Cancel:gtk-cancel)"
+
+IP="$dialog_input1"
+MP="$dialog_drop2"
+USER="$dialog_input2"
+PW="$dialog_password1"
+PORT="$dialog_input3"
+
+if [ X"$dialog_radio1" = "X1" ];then
+	TYPE="FTP"
+fi
+
+if [ X"$dialog_radio2" = "X1" ];then
+	TYPE="SSH"
+fi
+
+if [ X"$dialog_radio3" = "X1" ];then
+	TYPE="DAV"
+fi
+
+if [ X"$dialog_radio4" = "X1" ];then
+	TYPE="SMB"
+fi
+
+if [ "X$USER" != "X" ];then
+	SMBOPTIONS="-o username=$USER,password=$PW,uid=$UID,port=$PORT,vers=1.0"
+	FTPOPTIONS="${USER}:${PW}"
+else
+	SMBOPTIONS=""
+	FTPOPTIONS=""
+fi
+
+if [ "X$dialog_pressed" = "Xbutton1" ] && [ $TYPE = "SMB" ];then
+	echo "udevil mount -t cifs  $SMBOPTIONS //$IP/$MP" >>/tmp/mountshare.debug.log
+	udevil mount -t cifs  $SMBOPTIONS //$IP/$MP
+fi
+
+if [ "X$dialog_pressed" = "Xbutton1" ] && [ $TYPE = "FTP" ];then
+	echo "udevil mount -t curlftpfs ftp://$FTPOPTIONS@$IP" >>/tmp/mountshare.debug.log
+	udevil mount -t curlftpfs ftp://$FTPOPTIONS@$IP
+fi
+
+if [ "X$dialog_pressed" = "Xbutton1" ] && [ $TYPE = "SSH" ];then
+	#echo "echo ${PW}|udevil mount -t sshfs $IP -o port=$PORT,password_stdin" >>/tmp/mountshare.debug.log
+	#echo ${PW}|udevil mount -t sshfs $IP -o port=$PORT,password_stdin
+	echo ${PW}|sudo -S mkdir /media/ssh-${IP}
+	echo ${PW}|sudo -S chown $USER:$USER /media/ssh-${IP}
+	/usr/sbin/mount.sshfs ${USER}@${IP}:/ /media/ssh-${IP}
+fi
+
+if [ "X$dialog_pressed" = "Xbutton1" ] && [ $TYPE = "DAV" ];then
+	echo "echo -e "$USER\n$PW\n"|udevil mount -t davfs http://$IP:$PORT" >>/tmp/mountshare.debug.log
+	echo -e "$USER\n$PW\n"|udevil mount -t davfs http://$IP:$PORT
+fi
+
+echo -n "$PW" > "$PASSFILE"
+echo -n "$USER" > "$USERFILE"
+echo -n "$IP" > "$IPADDRESS"
+echo -n "$MP" > "$SELECTEDDISK"
+
+case $TYPE in
+	"FTP")
+		echo -n "$PORT" > "$PORTNUMFTP"
+		;;	
+	"SSH")
+		echo -n "$PORT" > "$PORTNUMSSH"
+		;;	
+	"SMB")
+		echo -n "$PORT" > "$PORTNUMSMB"
+		;;	
+	"DAV")
+		echo -n "$PORT" > "$PORTNUMDAV"
+		;;	
+esac
+
+exit $?
+
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SpaceFM-Plugins
 Plugins for the SpaceFM file manager
+
+INSTALLATION:
+
+cd to plugin folder, run makeplugin script and install plugin ( from /tmp ) via spacefm's Plugins->Install->File menu eg:
+
+cd MountShare
+./makeplugin
+spacefm->Plugins->Install->File ( /tmp/MountShare.spacefm-plugin.tar.gz )
+
+Enter root password whe prompted.
+Simple!
